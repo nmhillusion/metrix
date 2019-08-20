debdacce86b6c3cee6401ae079a0c56564cbee0c
nmhillusion <nguyenminhhieu.geek@gmail.com>
2019-08-20 10:35:31 +0700

    add random color and updated checking type nfn

--- js/function-utils.js
+++ js/function-utils.js
@@ -3,11 +3,23 @@ function fn(){
     nfn(fn, amount = 1e10){
       return (...args) => {
         amount = Math.min(args.length, amount);
-        return fn(...args.slice(0, amount));
+        return "function" == typeof fn ? fn(...args.slice(0, amount)) : null;
       }
     },
     suffle(arr){
       return arr;
+    },
+    randomColor(){
+      let result = "#";
+      do{
+        result += Math.round(Math.random() * 16).toString(16);
+      }while(7 < result.length);
+      return result;
+    },
+    log(o){
+      if("object" == typeof o){
+        Object.keys(o).forEach(key => console.log(` ${key} : ${o[key]} `));
+      }
     }
   }
 }
