82b66b25a7c515b9116bd5219eebe3f18d1e3040
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-07-31 20:23:47 +0700

    impl for argument start with -

--- src/typescript/modules/cli-params.parser.ts
+++ src/typescript/modules/cli-params.parser.ts
@@ -1,18 +1,98 @@
-export function cliParamsParser(argvs: string[]): Map<string, string> {
-  const paramMap: Map<string, string> = new Map();
+interface Pair {
+  key: string;
+  value: string | boolean;
+}
+
+/**
+ * Parser arguments from command line with structure of <command> <arg1> <arg2> <arg3>...
+ * Every argument will be parsed and splited by `=` to obtain value.
+ * If key has `--` in starting, it will be remove
+ * @param argvs arguments from command line
+ * @returns map of argument with key and values from 2 part of pair `<key>=<value>`
+ */
+export function cliParamsParser(
+  argvs: string[]
+): Map<string, string | boolean> {
+  const paramMap: Map<string, string | boolean> = new Map();
 
   if (Array.isArray(argvs)) {
-    for (const argv of argvs) {
-      const convArgv = String(argv);
-      if (convArgv.includes("=")) {
-        const [key, value] = convArgv.split("=");
+    for (const argvIdx in argvs) {
+      const argv = String(argvs[argvIdx]);
 
-        if (key.trim() && value.trim()) {
-          paramMap.set(String(key).trim(), String(value).trim());
-        }
+      let parsedItem: Pair | null = null;
+
+      if (argv.includes("=")) {
+        parsedItem = parseParamsWithEqualSymbol(argvs, argvIdx, argv);
+      } else if (argv.startsWith("-")) {
+        parsedItem = parseParamsWithDashSymbolStarting(argvs, argvIdx, argv);
+      }
+
+      if (parsedItem) {
+        paramMap.set(parsedItem.key, parsedItem.value);
       }
     }
   }
 
   return paramMap;
+}
+
+function parseParamsWithEqualSymbol(
+  argvs: string[],
+  argvIdx: string,
+  argv: string
+): Pair | null {
+  const [key, value] = argv.split("=");
+  let convertedKey = key;
+
+  while (convertedKey.startsWith("-") && 2 <= convertedKey.length) {
+    convertedKey = convertedKey.slice(1);
+  }
+
+  if (convertedKey.trim() && value.trim()) {
+    return {
+      key: String(convertedKey).trim(),
+      value: String(value).trim(),
+    };
+  }
+
+  return null;
+}
+
+function parseParamsWithDashSymbolStarting(
+  argvs: string[],
+  argvIdxRaw: string,
+  argv: string
+): Pair | null {
+  const argvIdx = Number(argvIdxRaw);
+  if (Number.isNaN(argvIdx)) {
+    return null;
+  }
+  if (argv.includes("=")) {
+    return null;
+  }
+  if (!argv.startsWith("-")) {
+    return null;
+  }
+
+  let convertedKey = argv;
+  while (convertedKey.startsWith("-") && 2 <= convertedKey.length) {
+    convertedKey = convertedKey.slice(1);
+  }
+
+  if (!convertedKey) {
+    return null;
+  }
+
+  let value: string | boolean = true;
+  const nextArgv =
+    argvIdx + 1 < argvs.length - 1 ? String(argvs[argvIdx + 1]) : null;
+
+  if (nextArgv && !nextArgv.startsWith("-") && !nextArgv.includes("=")) {
+    value = String(nextArgv);
+  }
+
+  return {
+    key: convertedKey,
+    value,
+  };
 }
--- test/cli-params.parser.test.ts
+++ test/cli-params.parser.test.ts
@@ -16,6 +16,56 @@ test("test with params", () => {
   expect(paramList.has("gender")).toBeFalsy();
 });
 
+test("test with params start with - and includes =", () => {
+  const argvs = ["node.exe", "d:/data/main.js", "-m=PROD", "--uid=10001"];
+
+  const paramList = cliParamsParser(argvs);
+
+  console.log({ paramList });
+
+  expect(paramList).toEqual(
+    new Map<string, string>().set("m", "PROD").set("uid", "10001")
+  );
+
+  expect(paramList.has("m")).toBeTruthy();
+  expect(paramList.has("uid")).toBeTruthy();
+  expect(paramList.has("weight")).toBeFalsy();
+});
+
+test("test with params start with - and not includes =", () => {
+  const argvs = [
+    "node.exe",
+    "d:/data/main.js",
+    "-v",
+    "2",
+    "--compressed",
+    "--uid=10001",
+    "phone=0922.893.234",
+    "-p",
+  ];
+
+  const paramList = cliParamsParser(argvs);
+
+  console.log({ paramList });
+
+  expect(paramList).toEqual(
+    new Map<string, string | boolean>()
+      .set("v", "2")
+      .set("compressed", true)
+      .set("uid", "10001")
+      .set("phone", "0922.893.234")
+      .set("p", true)
+  );
+
+  expect(paramList.has("v")).toBeTruthy();
+  expect(paramList.has("compressed")).toBeTruthy();
+  expect(paramList.has("uid")).toBeTruthy();
+  expect(paramList.has("phone")).toBeTruthy();
+  expect(paramList.has("p")).toBeTruthy();
+  expect(paramList.has("weight")).toBeFalsy();
+  expect(paramList.has("seri")).toBeFalsy();
+});
+
 test("test with no params", () => {
   const argvs = ["node.exe", "d:/data/main.js", "PROD", "thomas"];
 
