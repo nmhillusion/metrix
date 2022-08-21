4a2db79a689f23c677a7239ec64fcd958170b7c7
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-08-21 16:48:16 +0700

    fixed entry path of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./dist/javascript/modules/index.js",
+  entry: "./dist/javascript/index.js",
   output: {
     library: "_m",
     filename: "main.js",
