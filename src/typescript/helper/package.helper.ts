b1809038013425a2a3997cb2afca58436b7ca248
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-08-21 17:57:53 +0700

    fixed package helper: build publish package json

--- src/typescript/helper/package.helper.ts
+++ src/typescript/helper/package.helper.ts
@@ -9,19 +9,61 @@ export function buildPublishPackageJson({
   basePublishDir: string;
 }) {
   let packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+  const outputPackageJsonPath = path.join(basePublishDir, "package.json");
+  const packageDir = path.dirname(packageJsonPath);
+
+  if (!packageJson["main"]) {
+    throw new Error("Does not exist main entry in package.json");
+  }
 
   packageJson = Object.assign(packageJson, {
-    main: "index.js",
-    types: "index.d.ts",
+    main: getPublishPathOfEntry({
+      basePublishDir,
+      packageDir,
+      packageJson,
+      entryKeys: ["main"],
+    }),
+    types: getPublishPathOfEntry({
+      basePublishDir,
+      packageDir,
+      packageJson,
+      entryKeys: ["types", "typings"],
+    }),
     files: ["*"],
-    scripts: {},
   });
 
-  fs.writeFileSync(
-    path.join(basePublishDir, "package.json"),
-    JSON.stringify(packageJson),
-    {
-      encoding: "utf-8",
+  delete packageJson["scripts"];
+  delete packageJson["typings"];
+
+  fs.writeFileSync(outputPackageJsonPath, JSON.stringify(packageJson), {
+    encoding: "utf-8",
+  });
+}
+
+function getPublishPathOfEntry({
+  packageDir,
+  packageJson,
+  basePublishDir,
+  entryKeys,
+}: {
+  packageDir: string;
+  packageJson: { [entryKey: string]: any };
+  basePublishDir: string;
+  entryKeys: ("main" | "types" | "typings")[];
+}) {
+  let entryValue = "";
+  for (const entryKey of entryKeys) {
+    entryValue = packageJson[entryKey];
+    if (entryValue) {
+      break;
     }
-  );
+  }
+
+  if (!entryValue) {
+    return entryValue;
+  }
+
+  const realPathOfEntryFile = path.join(packageDir, entryValue);
+
+  return path.relative(basePublishDir, realPathOfEntryFile).replace("\\", "/");
 }
